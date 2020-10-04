8b90c5d00e45fa958b3c7401b0984711aab1990c
MedynskyyOstap <47416022+voloxatyj@users.noreply.github.com>
2020-10-04 09:05:58 +0300

    repair store-issue

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,23 +4,41 @@ import userReducer from './reducers/userReducer'
 import uiReducer from './reducers/uiReducer'
 import dataReducer from './reducers/dataReducer'
 
-const initialState = {}
+// const initialState = {}
 
-const middleware = [thunk] || [window.thunk.default]
+// const middleware = [thunk] || [window.thunk.default]
+
+// const reducers = combineReducers({
+// 	user: userReducer,
+// 	data: dataReducer,
+// 	ui: uiReducer
+// })
+
+// const store = createStore(
+// 	reducers,
+// 	initialState,
+// 	compose(
+// 		applyMiddleware(...middleware),
+// 		window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+// 	)
+// )
+
+// export default store
 
 const reducers = combineReducers({
-	user: userReducer,
-	data: dataReducer,
+	data: userReducer,
 	ui: uiReducer
 })
+
+const middleware = [
+	applyMiddleware(...[thunk] || [window.thunk.default]),
+	...(window.__REDUX_DEVTOOLS_EXTENSION__ ? [window.__REDUX_DEVTOOLS_EXTENSION__()] : [])
+]
 
 const store = createStore(
 	reducers,
 	initialState,
-	compose(
-		applyMiddleware(...middleware),
-		window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-	)
+	compose(...middleware)
 )
 
 export default store
